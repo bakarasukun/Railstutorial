901c975c816f63accba422d6b30477549907c424
bakarasukun <kujoukrs@gmail.com>
2019-09-20 18:23:04 +0900

    refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-    render html:"test"
+    render html: "test"
   end
+  
   def test
-      render html:"vector"
+      render html: "vector"
   end
 end
