a9612e0fd2788a56377813fc2e702e83220fbd51
bakarasukun <kujoukrs@gmail.com>
2019-10-16 16:50:43 +0900

    add password

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   
   def setup
-    @user = User.new(name: 'Example User', email: 'user@example.com')
+    @user = User.new(name: 'Example User', email: 'user@example.com',
+                    password: 'foobar', password_confirmation: 'foobar')
   end
 
   test 'should be valid' do
@@ -31,7 +32,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test 'email validation should accept valid addresses' do
-    valid_addresses = %w[user@example.com USER@foo.COM A_US_ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses = %w[user1000@example.com USER@foo.COM A_US_ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
     valid_addresses.each do |valid_address|
       @user.email = valid_address
       assert @user.valid?, "#{valid_address.inspect} should be valid"
@@ -51,5 +52,15 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user.email = @user.email.upcase 
     @user.save
     assert_not duplicate_user.valid?
+  end
+
+  test 'password should be present (nonblank)' do
+    @user.password = @user.password_confirmation = ' ' * 6
+    assert_not @user.valid?
+  end
+
+  test 'password should have a minimum length' do
+    @user.password = @user.password_confirmation = 'a' * 5
+    assert_not @user.valid?
   end
 end
