70bc8fb863e218b3794d8d3f210222210d9546ea
bakarasukun <kujoukrs@gmail.com>
2019-12-06 18:13:41 +0900

    add user edits

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class UsersEditTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
+    @other_user =users(:archer)
   end
 
   test 'unsuccessful edit' do
@@ -44,5 +45,37 @@ class UsersEditTest < ActionDispatch::IntegrationTest
                                         email: @user.email } }
     assert_not flash.empty?
     assert_redirected_to login_url
+  end
+
+  test 'should redirect edit when logged in as wrong user' do
+    log_in_as(@other_user)
+    get edit_user_path(@user)
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test 'should redirect update when logged in as wrong user' do
+    log_in_as(@other_user)
+    patch user_path(@user), params: { user: { name: @user.name,
+                                email: @user.email } }
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test 'successful edit with friendly fowarding' do
+    get edit_user_path(@user)
+    log_in_as(@user)
+    assert_redirected_to edit_user_path(@user)
+    name = 'Foo Bar'
+    email = 'foo@bar.com'
+    patch user_path(@user), params: { user:{ name: name,
+                                        email: email,
+                                        password: '',
+                                        password_confirmation: '' } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal @user.name, name
+    assert_equal @user.email, email
   end
 end
