cb22b085bc6f2ccf8e5bdd5cbc5b071259d1d7ff
bakarasukun <kujoukrs@gmail.com>
2019-12-25 16:44:57 +0900

    make microposts test

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -16,7 +16,7 @@ class MicropostsController < ApplicationController
   def destroy
     @micropost.destroy
     flash[:success] = 'Micropsot deleted'
-    redirect_to requset.referrer || root_url
+    redirect_to request.referrer || root_url
   end
 
   private
@@ -26,7 +26,7 @@ class MicropostsController < ApplicationController
     end
 
     def correct_user
-      @micropost = current_user.micropost.find_by(id: params[:id])
+      @micropost = current_user.microposts.find_by(id: params[:id])
       redirect_to root_url if @micropost.nil?
     end
 end
