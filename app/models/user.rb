5d676af0c519cf16d7d447befa75bbb7e1124faf
bakarasukun <kujoukrs@gmail.com>
2020-02-12 17:18:44 +0900

    sanitize

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,8 @@ class User < ApplicationRecord
 
   def feed
     following_ids = 'SELECT followed_id FROM relationships WHERE follower_id = :user_id'
-    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id",user_id: id)
+    serch = ActiveRecord::Base.send(:sanitize_sql_array, ["user_id IN (#{following_ids}) OR user_id = :user_id",user_id: id])
+    Micropost.where(serch)
   end
 
   def follow(other_user)
