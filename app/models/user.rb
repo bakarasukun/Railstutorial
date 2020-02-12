3f8f210a3506846d2f3cc24c3ec22d9fe1716eb1
bakarasukun <kujoukrs@gmail.com>
2020-02-12 15:36:02 +0900

    add follow/unfollow interface

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,12 @@ class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
   has_many :active_relationships, class_name: 'Relationship',
                   foreign_key: 'follower_id',
-                  dependent: destroy
+                  dependent: :destroy
+  has_many :passive_relationships, class_name: 'Relationship',
+                  foreign_key: 'followed_id',
+                  dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -12,14 +17,25 @@ class User < ApplicationRecord
 
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-  
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ?BCrypt::Engine::MIN_COST:
+  def self.digest(s)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                               BCrypt::Engne.cost
-    BCrypt::Password.create(string, cost: cost)
+    BCrypt::Password.create(s, cost: cost)
   end
 
   def feed
     Micropost.where('user_id=?', id)
+  end
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 end
